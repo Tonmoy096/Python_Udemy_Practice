a905da8e28ed4d1081d10218d271f4dba6d4cbe5
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-06 02:58:29 +0600

    Add initial implementation of functions in video12.py

---
+++ video12.py
@@ -0,0 +1,27 @@
+# Functions
+
+def great():
+    print("Hi! python is a programming")
+
+
+great()
+
+"""
+
+def
+great_user(name):
+
+    return "Hello," + name + "!"
+
+
+message = great_user("Martha")
+print(message)
+"""
+
+
+def newgreat_user(name="Python is a great language"):
+    return "hello," + name + "!"
+
+
+message = newgreat_user()
+print(message)
