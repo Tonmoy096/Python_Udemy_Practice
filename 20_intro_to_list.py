6e860c239d6b012f786ce62b058c4e64a0efaa7b
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-06 20:42:46 +0600

    Add initial implementation of list operations with examples for fruit manipulation

---
+++ 20_intro_to_list.py
@@ -0,0 +1,17 @@
+# intro to list
+
+fruits = ["apple", "Banana", "Orange", "Grapes"]
+print(fruits)
+print(type(fruits))
+print(len(fruits))
+
+print(fruits[0])
+print(fruits[1])
+
+
+fruits[1] = "Mango"
+fruits.append("Pineapple")
+print(fruits)
+
+subset = fruits[1:4]
+print(subset)
