a0234e1cd3716bc323350eef7c20bee5daf278b7
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-06 15:05:00 +0600

    Add flow control examples with if, elif, else and for loop

---
+++ 15_intro_to_flow_control.py
@@ -0,0 +1,27 @@
+# flow control
+
+# if, elif, else
+
+
+x = 10
+
+if x > 5:
+    print("x is greater than 5")
+
+
+y = 3
+
+if y > 5:
+    print("y is greater than 5")
+
+elif y == 3:
+    print("y is equal to 3")
+
+else:
+    print("y is smaller")
+
+
+# for loop
+
+for i in range(8):
+    print(i)
