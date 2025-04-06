fefa587463996c3488af4bec6d237a1769442167
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-07 00:44:41 +0600

    Add initial implementation of list and string operations with examples

---
+++ 23_list_vs_string.py
@@ -0,0 +1,23 @@
+# list and strings
+
+
+language = ["Python", "Java", "C++", "JavaScript"]
+
+print(language)
+
+string = "Hello!"
+
+print(language[0])
+
+print(string[1])
+
+# list mutable
+# string immutable
+
+language[1] = 'c#'
+
+print(language)
+
+
+new_string = string+"word"
+print(new_string)
