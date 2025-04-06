d825ec19e5fc69692a470e8f09c7f408d4bc8d41
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-06 15:18:08 +0600

    Add implementation of nested if-else statements for comparison and number evaluation

---
+++ 16_nested_if_&_else.py
@@ -0,0 +1,25 @@
+# nested if and else
+
+x = 30
+y = 30
+
+if x > y:
+    print(" x is greater than y")
+
+else:
+    if x < y:
+        print("x is less than y")
+    else:
+        print("x is equal to y")
+
+
+num = 10
+
+if num > 0:
+    print("The value is a positive number")
+
+else:
+    if num < 0:
+        print("Negative number")
+    else:
+        print("zero")
