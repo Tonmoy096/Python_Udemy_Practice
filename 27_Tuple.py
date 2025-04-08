0fdc24cdc10bd1e661a514f7e8f2fdabd02b4576
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-08 16:05:17 +0600

    Add initial implementation of tuple operations with examples

---
+++ 27_Tuple.py
@@ -0,0 +1,16 @@
+# Intro to tuple
+
+my_tuple = (1, 2, 3, 4, 5, 6)
+print("Tuple:", my_tuple)
+
+mixed_tuple = (1, 2, "Nothinh", 3.5, True, None)
+print("Mixed Tuples:", mixed_tuple)
+
+print("First elements of mixed tuple:", mixed_tuple[0])
+
+print("Last elements of mixed tuples:", mixed_tuple[-1])
+
+print("The first element of tuple:", my_tuple[0])
+
+subset = my_tuple[2:4]
+print("Subset:", subset)
