489e90e107559de51fc6a3439111b93dd7a39c41
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-08 16:31:45 +0600

    Add implementation of tuple methods with examples

---
+++ 30_tuple_method.py
@@ -0,0 +1,18 @@
+# Tuple method
+
+
+my_tuple = (1, 2, 3, 4, 5, 3, 2, 8)
+
+index_of_three = my_tuple.index(3)
+
+print(index_of_three)
+
+count_of_three = my_tuple.count(3)
+print(count_of_three)
+
+
+tuple_length = len(my_tuple)
+print("Length of tuple:", tuple_length)
+
+sorted_tuple = sorted(my_tuple)
+print("Sorted tuple:", sorted_tuple)
