bd7ec246b97202d3245d073d5c552e307f1b3cdd
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-08 16:25:08 +0600

    Add implementation of tuple joining and repetition examples

---
+++ 29_Join_tuple.py
@@ -0,0 +1,18 @@
+# join tuple
+
+
+tuple1 = (1, 2, 3)
+tuple2 = ("a", "b", "c")
+
+joined_tuple = tuple1 + tuple2
+
+print(joined_tuple)
+
+
+tuple1 += tuple2
+
+print("Updated tuple:", tuple1)
+
+repeated_tuple = tuple1 * 3
+
+print("Tuples repeated 3 times:", repeated_tuple)
