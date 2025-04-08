b26d816989dd9a1ca5e463697e4a4f92f0240b45
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-08 16:12:51 +0600

    Add initial implementation of tuple access examples

---
+++ 28_Access_tuple.py
@@ -0,0 +1,17 @@
+# access tuple
+
+my_tuple = ("apple", "banana", "orange", "dates")
+
+print("First element of tuple:", my_tuple[0])
+
+print("Second elemenet of my tuple:", my_tuple[1])
+
+print("Last element of my tuple:", my_tuple[-1])
+
+
+subset = my_tuple[1:3]
+
+print("Subset:", subset)
+
+print("second last element:", my_tuple[-2])
+print("Last two element of my tuple:", my_tuple[-2:])
