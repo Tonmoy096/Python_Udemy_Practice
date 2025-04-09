d433290d6161babd66572f9c423feeadc5107490
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-09 22:31:55 +0600

    Add implementation of set methods with examples

---
+++ 34_sets_method.py
@@ -0,0 +1,19 @@
+# sets method
+
+my_set = {1, 2, 3, 4, 5, 6}
+my_set.add(7)
+print("Here is the updated set:", my_set)
+my_set.add(1)
+print("Here is the updated set:", my_set)
+
+my_set.remove(3)
+print("After removing 3 my updated set is :", my_set)
+my_set.discard(2)
+print("After removing 2 my updated set is :", my_set)
+
+my_set.update([8, 9, 10])  # i can use both curly bracees and square braces
+print("After updating my set is :", my_set)
+
+
+pop_element = my_set.pop()
+print("Here is the popped element:", pop_element)
