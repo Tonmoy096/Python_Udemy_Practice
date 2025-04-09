4725cf26262967c88a6f68958f287eeb9b7d53f3
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-09 22:13:21 +0600

    Add example usage of sets with union operation

--- 31_intro_to_set.py
+++ 31_intro_to_set.py
@@ -2,3 +2,14 @@
 
 # set is a collection of unique elements
 # set is unordered
+
+
+my_set = {1, 2, True, "tonmoy"}
+
+print(my_set)
+
+set1 = {1, 2, 3, 4, 5, 6}
+set2 = {5, 6, 7, 8, 9, 10}
+
+union_set = set1 | set2
+print("Here is the union set:", union_set)
