0aa9b03879de2004611815aa54a3ebb79d723bdb
Tonmoy096 <foysalkabir2017@gmail.com>
2025-04-15 00:06:27 +0600

    Refactor dictionary operations to include deletion and update examples

--- 26_Get_keys_values_items_update_pop_clear.py
+++ 26_Get_keys_values_items_update_pop_clear.py
@@ -6,3 +6,12 @@ my_dict = {
 
 apple_quantity = my_dict.get("apple")
 print("The quantity of the apple is:", apple_quantity)
+
+del my_dict['apple']
+print("After removing apple:", my_dict)
+
+print("The quantity of the apple is:", my_dict.get("apple"))
+print("The quantity of the banana is:", my_dict.get("banana"))
+
+my_dict.update({"kiwi": 100})
+print(my_dict)
